956eeb424df34761568ae6e27c77036aa0cb4316
cfryerdev <cfryerdev@gmail.com>
2020-04-22 10:05:19 -0600

    2 files modified, 8 added, 2 deleted

---
+++ src/app/store/store.ts
@@ -0,0 +1,16 @@
+// import { combineReducers } from 'redux';
+
+// import { ItemsReducer } from '../reducers/item.reducer';
+// import { Item } from '../models/account.model';
+// import { AccountReducer } from '../reducers/account.reducer';
+// import { Account } from '../models/account.model';
+
+// export class IAppState {
+//   needs: Item[];
+//   sharing: Item[];
+//   account: Account;
+// };
+
+// export const rootReducer = combineReducers<IAppState>({
+
+// });
